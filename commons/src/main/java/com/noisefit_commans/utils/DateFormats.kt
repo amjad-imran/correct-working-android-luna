a91188d3be65df1d8d3111e8bced8a88e969fad2
deepak.k2 <deepak.k@nexxbase.com>
2023-08-07 14:27:26 +0530

    epoc time in header

--- commons/src/main/java/com/noisefit_commans/utils/DateFormats.kt
+++ commons/src/main/java/com/noisefit_commans/utils/DateFormats.kt
@@ -899,6 +899,7 @@ object DateFormats {
         return SimpleDateFormat("yyyy-MM-ddHH:mm", defaultLocale).parse("$date$time").time
 
     }
+
     fun convertDateTimeToTimeStamp3(dateTime: String): Long {
         return dateTimeFormat5.parse(dateTime).time
 
@@ -1065,7 +1066,7 @@ object DateFormats {
         val calendar = Calendar.getInstance()
 
         //first day of week
-        calendar[Calendar.WEEK_OF_YEAR] = (week - 1)
+        calendar.set(Calendar.WEEK_OF_YEAR, week)
 
         val formatter = SimpleDateFormat("dd", Locale.getDefault()) // PST`
         calendar.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY)
