37a06989509e93488eb6046c907fd80f8659e979
deepak.k2 <deepak.k@nexxbase.com>
2023-08-14 17:40:07 +0530

    Privacy url change

--- commons/src/main/java/com/noisefit_commans/utils/AppConstants.kt
+++ commons/src/main/java/com/noisefit_commans/utils/AppConstants.kt
@@ -6,7 +6,7 @@ object AppConstants {
     const val OTP_RESEND_TIMER = 60000L
 
     const val URL_CONTACT_SUPPORT = "https://www.gonoise.com/pages/register-your-complaint"
-    const val URL_PRIVACY_POLICY = "https://www.gonoise.com/pages/app-privacy-policy"
+    const val URL_PRIVACY_POLICY = "https://www.gonoise.com/pages/luna-ring-privacy-policy"
     const val URL_TERMS_OF_USE = "https://www.gonoise.com/pages/terms-of-use"
     const val URL_GOOGLE_FIT = "https://www.google.com/fit"
     const val URL_WARRANTY_REGISTRATION = "https://www.gonoise.com/pages/warranty-registration"
