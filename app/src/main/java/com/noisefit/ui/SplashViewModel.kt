ff8a60481d24a451ef4f1218f9ba262cc4ed2616
deepak.k2 <deepak.k@nexxbase.com>
2023-08-14 16:29:34 +0530

    Profile setup cases

--- app/src/main/java/com/noisefit/ui/SplashViewModel.kt
+++ app/src/main/java/com/noisefit/ui/SplashViewModel.kt
@@ -276,7 +276,10 @@ class SplashViewModel
 
     private fun isProfileSetupComplete(): Boolean {
         val user = localDataStore.getUser() ?: return false
-        if (user.userInfo?.dob.isNullOrEmpty() && user.userInfo?.height == 0 && user.userInfo?.weight == 0) {
+        if (user.userInfo?.dob.isNullOrEmpty() || (user.userInfo?.height
+                ?: 0) == 0 || (user.userInfo?.weight ?: 0) == 0 || (user.userGoals?.caloriesGoal
+                ?: 0) == 0
+        ) {
             return false
         }
         return true
