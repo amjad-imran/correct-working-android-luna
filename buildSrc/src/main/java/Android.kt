df9a1db991fd5e53b0ef887a3508c88bf313c5a1
akhilendra.chauhan2 <akhilendra.chauhan@nexxbase.com>
2023-08-17 18:35:41 +0530

    version change

--- buildSrc/src/main/java/Android.kt
+++ buildSrc/src/main/java/Android.kt
@@ -6,6 +6,6 @@ object Android {
     const val ndk = "23.1.7779620"
     const val minSdk = 23
     const val targetSdk = 32
-    const val versionCode = 2
+    const val versionCode = 4
     const val versionName = "1.0.0"
 }
