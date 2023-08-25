749b7f86bde9b642c7fb3b2a404efdf87b542c29
deepak.k2 <deepak.k@nexxbase.com>
2023-08-25 17:15:57 +0530

    local.prop

--- buildSrc/src/main/java/Android.kt
+++ buildSrc/src/main/java/Android.kt
@@ -7,5 +7,5 @@ object Android {
     const val minSdk = 23
     const val targetSdk = 33
     const val versionCode = 1
-    const val versionName = "1.0.16"
+    const val versionName = "1.0.17"
 }
