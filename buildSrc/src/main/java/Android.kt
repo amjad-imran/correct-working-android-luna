7400a9f39f293cff9b94cebadb223a307642f829
akhilendra.chauhan2 <akhilendra.chauhan@nexxbase.com>
2023-08-27 14:25:11 +0530

    version change

--- buildSrc/src/main/java/Android.kt
+++ buildSrc/src/main/java/Android.kt
@@ -2,7 +2,7 @@
 
 object Android {
     const val appId = "com.noisefit.luna"
-    const val compileSdk = 32
+    const val compileSdk = 33
     const val ndk = "23.1.7779620"
     const val minSdk = 23
 
