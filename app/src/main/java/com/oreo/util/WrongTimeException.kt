f4befa670ae36800304fb96bdcd4ba030184ea67
deepakypro <deepak.yadav@nexxbase.com>
2023-09-01 15:34:20 +0530

    wrong time dialog

--- app/src/main/java/com/oreo/util/WrongTimeException.kt
+++
@@ -1,9 +0,0 @@
-package com.oreo.util
-
-
-class WrongTimeException : Exception {
-    constructor() : super()
-    constructor(message: String) : super(message)
-    constructor(message: String, cause: Throwable) : super(message, cause)
-    constructor(cause: Throwable) : super(cause)
-}
