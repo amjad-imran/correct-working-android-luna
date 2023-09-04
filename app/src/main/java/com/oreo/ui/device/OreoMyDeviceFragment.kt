2ba09592161e0aabd8d9a33ece18a0f53feb3b80
deepak.k2 <deepak.k@nexxbase.com>
2023-09-04 11:19:40 +0530

    device features flow

--- app/src/main/java/com/oreo/ui/device/OreoMyDeviceFragment.kt
+++ app/src/main/java/com/oreo/ui/device/OreoMyDeviceFragment.kt
@@ -37,8 +37,8 @@ class OreoMyDeviceFragment :
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //binding.features = mViewModel.localDataStore.getDeviceFeatures()
-        binding.features = DeviceFeatures(shareLogs = 1)
+        binding.features = mViewModel.ringDataStore.getDeviceFeatures()
+        //binding.features = DeviceFeatures(shareLogs = 1)
 
         binding.lifecycleOwner = this
     }
