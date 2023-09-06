eddd081afca575543a962dd101727323a39d556a
deepak.k2 <deepak.k@nexxbase.com>
2023-09-06 17:39:14 +0530

    Remove dash and activity offline data on workout delete

--- app/src/main/java/com/oreo/data/repository/implementation/OreoUserActivityRepositoryImpl.kt
+++ app/src/main/java/com/oreo/data/repository/implementation/OreoUserActivityRepositoryImpl.kt
@@ -1008,6 +1008,8 @@ class OreoUserActivityRepositoryImpl(
     override suspend fun deleteWorkoutFromServer(id: String): Flow<Resource<BaseApiResponse<Any>>> {
         //'https://stage-oreo.gonoise.com/activity/v1/delete_workout/e900fe86-f2d5-422f-be40-9d0d633caa19
         return safeApiCallFlow(dispatcher) {
+            keyValueDataSource.removeDataByType(KeyValueDataType.ACTIVITY)
+            keyValueDataSource.removeDataByType(KeyValueDataType.DASHBOARD)
             val url = "${BuildConfig.OREO_BASE_URL}/activity/v1/delete_workout/$id"
             remoteDataSource.deleteWorkout(url)
         }
