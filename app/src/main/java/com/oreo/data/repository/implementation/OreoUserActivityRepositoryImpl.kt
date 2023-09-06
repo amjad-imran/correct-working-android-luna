50117dd998ee65d0675e54197c5ceadc6fe4a8ca
deepak.k2 <deepak.k@nexxbase.com>
2023-09-06 20:47:28 +0530

    remove on adding workout

--- app/src/main/java/com/oreo/data/repository/implementation/OreoUserActivityRepositoryImpl.kt
+++ app/src/main/java/com/oreo/data/repository/implementation/OreoUserActivityRepositoryImpl.kt
@@ -951,6 +951,8 @@ class OreoUserActivityRepositoryImpl(
     override suspend fun addWorkout(request: JsonObject): Flow<Resource<BaseApiResponseData<Any>>> {
         return safeApiCallFlow(dispatcher) {
             val url = "${BuildConfig.OREO_BASE_URL}/activity/v1/add_workout"
+            keyValueDataSource.removeDataByType(KeyValueDataType.ACTIVITY)
+            keyValueDataSource.removeDataByType(KeyValueDataType.DASHBOARD)
             remoteDataSource.addWorkout(url, request)
         }
     }
