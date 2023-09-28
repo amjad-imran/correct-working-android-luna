e8d841277f8e2e725021c78292ba0c3cf4e93479
deepak.k2 <deepak.k@nexxbase.com>
2023-09-28 14:09:38 +0530

    OS-691 - hrr removed

--- app/src/main/java/com/oreo/ui/readiness/OreoReadinessViewModel.kt
+++ app/src/main/java/com/oreo/ui/readiness/OreoReadinessViewModel.kt
@@ -314,7 +314,7 @@ constructor(
         descriptionList.add(contributorInfo.value?.activityBalance ?: "")
         descriptionList.add(contributorInfo.value?.hrvBalance ?: "")
         descriptionList.add(contributorInfo.value?.restingHr ?: "")
-        descriptionList.add(contributorInfo.value?.heartRate ?: "")
+        //descriptionList.add(contributorInfo.value?.heartRate ?: "")
         descriptionList.add(contributorInfo.value?.recoveryIndex ?: "")
         return descriptionList
     }
@@ -494,7 +494,7 @@ constructor(
             )
         }
 
-        if (dayData?.hrReserve != null) {
+       /* if (dayData?.hrReserve != null) {
             val (textColor, barColor, background) = getContributorsColors(dayData.hrReserve.status)
 
             result.add(
@@ -518,7 +518,7 @@ constructor(
                     backgroundRes = com.noisefit_commans.R.drawable.back_modal_new
                 )
             )
-        }
+        }*/
 
         if (dayData?.recoveryIndex != null) {
             val (textColor, barColor, background) = getContributorsColors(dayData.recoveryIndex.status)
