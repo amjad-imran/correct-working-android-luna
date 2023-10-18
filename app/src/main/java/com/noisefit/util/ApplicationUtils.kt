c40c986e4b5f474bdebc085adae855a34250f30c
deepak.k2 <deepak.k@nexxbase.com>
2023-10-18 17:00:52 +0530

    OS-769

--- app/src/main/java/com/noisefit/util/ApplicationUtils.kt
+++ app/src/main/java/com/noisefit/util/ApplicationUtils.kt
@@ -783,7 +783,7 @@ object ApplicationUtils {
             }
         }
         bmrValue = (defValue1.plus(defValue2.times(weightValue)).plus(defValue3.times(heightValue))
-            .minus(defValue4.times(age))).times(0.3f).toDouble()
+            .minus(defValue4.times(age))).toDouble()
         return roundNearestValue(
             bmrValue.toFloat().roundToNearestDecimalFlooor(bmrValue.toFloat()).toDouble(), 10.0
         )
