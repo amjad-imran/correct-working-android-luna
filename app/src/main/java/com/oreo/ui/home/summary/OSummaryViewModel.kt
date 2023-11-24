73ae0f84a5f22510a2293598771e26d7809ea5c7
deepak.k2 <deepak.k@nexxbase.com>
2023-11-24 17:35:39 +0530

    sleep title update

--- app/src/main/java/com/oreo/ui/home/summary/OSummaryViewModel.kt
+++ app/src/main/java/com/oreo/ui/home/summary/OSummaryViewModel.kt
@@ -190,7 +190,7 @@ constructor(
                     pushNotification.postValue(
                         Event(
                             PushLocalNotification(
-                                "Sleep",
+                                "Good sleep last night",
                                 "You got enough sleep hours today. This helps with higher recovery, cognitive & immune system function",
                                 NotificationEventsClass.LOCAL_SLEEP_NOTIFICATION_KEY
                             )
