369ee7f9cb84d5637220d348d82f419f160a8e3a
deepak.k2 <deepak.k@nexxbase.com>
2023-12-14 14:58:47 +0530

    migration version change

--- app/src/main/java/com/oreo/data/db/OreoDataBase.kt
+++ app/src/main/java/com/oreo/data/db/OreoDataBase.kt
@@ -34,7 +34,7 @@ import com.oreo.data.db.database.OreoUserHealthDataDao
         OreoBloodPressureData::class, OreoSleepData::class, OreoStressDataBreakup::class, OreoGoogleFitData::class,
         OreoBodyTemperatureBreakup::class, OreoRespiratoryData::class, DayTimeMovementBreakup::class,
         OreoAutoSportData::class, KeyValue::class,UserHealthData::class],
-    version = 3, exportSchema = false
+    version = 4, exportSchema = false
 )
 @TypeConverters(Converters::class)
 abstract class OreoDataBase : RoomDatabase() {
