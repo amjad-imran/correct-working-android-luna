71b84da278c3a646dfe019cd6fe6de103bafefc5
deepak.k2 <deepak.k@nexxbase.com>
2023-12-29 16:52:14 +0530

    Blood oxygen saturation

--- app/src/main/java/com/oreo/ui/sleep/OreoSleepDetailFragment.kt
+++ app/src/main/java/com/oreo/ui/sleep/OreoSleepDetailFragment.kt
@@ -249,6 +249,28 @@ class OreoSleepDetailFragment :
         )
     }
 
+    private fun showAverageBloodOxygen(
+        oxy: CommonListDataModel?
+    ) {
+        if ((oxy?.avg ?: 0) == 0) {
+            binding.lytAverageBloodOxygen.root.gone()
+            return
+        }
+
+        binding.lytAverageBloodOxygen.tvNudge.text = viewModel.getBloodOxygenNudge(oxy)
+
+        if ((oxy?.avg ?: 0) < 95) {
+            binding.lytAverageBloodOxygen.root.visible()
+            binding.lytAverageBloodOxygen.tvAvgValue.text = "<95"
+            return
+        }
+        binding.lytAverageBloodOxygen.root.visible()
+        binding.lytAverageBloodOxygen.tvAvgValue.text = (oxy?.avg ?: 0).toString()
+
+
+
+    }
+
 
     private fun showHeartRateGraph(
         heartRateList: CommonListDataModel?,
@@ -439,6 +461,14 @@ class OreoSleepDetailFragment :
             }
         }
         binding.lytBloodOxygenGraph.bInfo.setOnClickListener {
+            viewModel.contributorInfo.value?.oxy_graph?.let { content ->
+                navigate(R.id.bottomSheetDataMetrics, Bundle().apply {
+                    this.putString("infoData", content)
+                })
+            }
+        }
+
+        binding.lytAverageBloodOxygen.bInfo.setOnClickListener {
             viewModel.contributorInfo.value?.oxy_graph?.let { content ->
                 navigate(R.id.bottomSheetDataMetrics, Bundle().apply {
                     this.putString("infoData", content)
@@ -904,6 +934,8 @@ class OreoSleepDetailFragment :
         showHeartRateVariabilityGraph(dayData.hrv, sleepStartTime, sleepEndTime)
 
         //showBloodOxygenGraph(dayData.oxy, sleepStartTime, sleepEndTime)
+
+        showAverageBloodOxygen(dayData.oxy)
 
         mSleepStageAdapter.setData(viewModel.getStepAnalysisData(dayData))
         initSleepAnalysisGraph(dayData.hourly_breakup)
