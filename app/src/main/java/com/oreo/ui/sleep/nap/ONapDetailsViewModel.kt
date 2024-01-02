53a7dab4bae2aac4e63fcfae6e84178da97ed945
Anjum Asif <asif.anjum@nexxbase.com>
2024-01-02 11:27:21 +0530

    nap details ui- inprogress

---
+++ app/src/main/java/com/oreo/ui/sleep/nap/ONapDetailsViewModel.kt
@@ -0,0 +1,10 @@
+package com.oreo.ui.sleep.nap
+
+import com.noisefit_commans.ui.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ONapDetailsViewModel @Inject constructor(): BaseViewModel() {
+
+}
