668ee10d180fac9b57dfbc97f8de8ce7c9cfa74b
deepak.k2 <deepak.k@nexxbase.com>
2024-01-15 17:32:36 +0530

    my device bottom sheet removed

--- app/src/main/java/com/oreo/ui/home/summary/OSummaryFragment.kt
+++ app/src/main/java/com/oreo/ui/home/summary/OSummaryFragment.kt
@@ -230,7 +230,7 @@ class OSummaryFragment : BaseFragment<FragmentSummaryOBinding>(FragmentSummaryOB
         }
 
 
-        //handle device intro
+       /* //handle device intro
         if (!mainViewModel.ringDataStore.isShowDeviceIntro()) {
             setFragmentResultListener(CALL_GOT_IT) { _, bundle ->
                 val isSelected = bundle.getBoolean("isSelected")
@@ -239,7 +239,7 @@ class OSummaryFragment : BaseFragment<FragmentSummaryOBinding>(FragmentSummaryOB
                 }
             }
             navigate(R.id.myDeviceIntroBottomSheet)
-        }
+        }*/
 
     }
 
