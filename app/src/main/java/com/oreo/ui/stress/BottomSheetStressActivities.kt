e3fe7a5544be9bb690ebd58fdfa6c8b2738700b0
deepak.k2 <deepak.k@nexxbase.com>
2024-02-13 18:19:39 +0530

    margin bottom

--- app/src/main/java/com/oreo/ui/stress/BottomSheetStressActivities.kt
+++ app/src/main/java/com/oreo/ui/stress/BottomSheetStressActivities.kt
@@ -2,17 +2,27 @@ package com.oreo.ui.stress
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.viewModels
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.noisefit.luna.R
 import com.noisefit.luna.databinding.BottomSheetStressActivitiesBinding
+import com.noisefit.oreo.BottomNavOption
+import com.noisefit.oreo.OreoMainViewModel
 import com.noisefit_commans.ui.BaseBottomSheetWithTransparent
+import com.noisefit_commans.ui.showShortToast
 import com.oreo.data.model.OStressActivitiesDataModel
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class BottomSheetStressActivities :
     BaseBottomSheetWithTransparent<BottomSheetStressActivitiesBinding>(
         BottomSheetStressActivitiesBinding::inflate
     ) {
+
+    private val viewModel: OreoMainViewModel by viewModels()
 
     private val dataList = ArrayList<OStressActivitiesDataModel>()
     val args: BottomSheetStressActivitiesArgs by navArgs()
@@ -29,9 +39,9 @@ class BottomSheetStressActivities :
                         putInt("position", -1)
                     })
                 } else if (data.type.equals("sleep", true)) {
-
+                    //viewModel.navigateTo(BottomNavOption.SLEEP)
                 } else if (data.type.equals("nap", true)) {
-
+                    //viewModel.navigateTo(BottomNavOption.SLEEP)
                 }
             }
         })
