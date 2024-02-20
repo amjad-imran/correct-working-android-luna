9c53bac7a72206a80aea7b09c411660f2139fc37
Anjum Asif <asif.anjum@nexxbase.com>
2024-02-20 18:16:08 +0530

    comment pair later

--- app/src/main/java/com/noisefit/ui/onboarding/pairing/find/FindDeviceListFragment.kt
+++ app/src/main/java/com/noisefit/ui/onboarding/pairing/find/FindDeviceListFragment.kt
@@ -351,14 +351,14 @@ class FindDeviceListFragment :
             }
 
         }
-        if (BuildConfig.DEBUG) {
+       /* if (BuildConfig.DEBUG) {
             binding.bPairLater.visible()
         } else
             binding.bPairLater.gone()
         binding.bPairLater.setOnClickListener {
             pairLater()
         }
-
+*/
         /*binding.layoutSearchAgain.root.setOnClickListener {
             checkPermissionAndScan()
         }*/
