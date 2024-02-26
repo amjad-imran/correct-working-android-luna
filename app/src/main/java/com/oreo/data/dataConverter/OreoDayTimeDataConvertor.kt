f908b5fc97299bac7022f5a797a2034c84825a02
deepak.k2 <deepak.k@nexxbase.com>
2024-02-26 20:04:07 +0530

    nap handling

--- app/src/main/java/com/oreo/data/dataConverter/OreoDayTimeDataConvertor.kt
+++ app/src/main/java/com/oreo/data/dataConverter/OreoDayTimeDataConvertor.kt
@@ -54,13 +54,15 @@ class OreoDayTimeDataConvertor @Inject constructor() {
         }
 
         dayData.sleep?.naps?.forEach { nap ->
-            getNapSection(nap)?.let {
-                Section(
-                    "nap",
-                    it.first,
-                    it.second,
-                    Color.parseColor("#4cc5a8ed"),
-                    R.drawable.icon_stress_sleep
+            getNapSection(nap, dayData.date)?.let {
+                sections.add(
+                    Section(
+                        "nap",
+                        it.first,
+                        it.second,
+                        Color.parseColor("#4cc5a8ed"),
+                        R.drawable.icon_stress_sleep
+                    )
                 )
             }
         }
@@ -190,13 +192,15 @@ class OreoDayTimeDataConvertor @Inject constructor() {
         return Triple(startPos, calculatedDuration.toInt(), it.iconUrl)
     }
 
-    private fun getNapSection(nap: Nap): Pair<Int, Int>? {
+    private fun getNapSection(nap: Nap, date: String): Pair<Int, Int>? {
 
         val startTime = nap.startTime
         val endTime = nap.endTime
 
         val sleepStartDate = startTime.split(" ")[0]
         val sleepEndDate = endTime.split(" ")[0]
+
+        if (!sleepStartDate.equals(date)) return null
 
         if (sleepStartDate.equals(sleepEndDate)) {
             //Same day Sleep
