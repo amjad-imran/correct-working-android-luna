abfc1a4a036fe41fe756bb01c91e9a07c5934a5f
deepak.k2 <deepak.k@nexxbase.com>
2024-02-27 13:09:29 +0530

    daytime top graph space handling

--- app/src/main/java/com/oreo/ui/custom/ODayTimeInteractiveGraph.kt
+++ app/src/main/java/com/oreo/ui/custom/ODayTimeInteractiveGraph.kt
@@ -265,6 +265,12 @@ class ODayTimeInteractiveGraph : View {
                 list.addAll(it)
             }
         }
+        if (dayTimeDataModel?.sections.isNullOrEmpty()) {
+            topWith = 0f
+        } else {
+            topWith = dpToPx(40, context)
+        }
+
         postInvalidate()
     }
 
