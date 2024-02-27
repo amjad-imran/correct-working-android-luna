1dfd94ad7bf808c33e424751d1bf4e292aab9858
deepak.k2 <deepak.k@nexxbase.com>
2024-02-27 10:38:01 +0530

    version change

--- buildSrc/src/main/java/Android.kt
+++ buildSrc/src/main/java/Android.kt
@@ -7,6 +7,6 @@ object Android {
     const val minSdk = 23
 
     const val targetSdk = 33
-    const val versionCode = 70
-    const val versionName = "1.1.4"
+    const val versionCode = 72
+    const val versionName = "1.1.6"
 }
