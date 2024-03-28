cd6864cd4f4ec8771b4778991c1069ec46767751
deepakypro <deepak.yadav@nexxbase.com>
2024-03-28 16:33:24 +0530

    delete unused files

--- app/src/main/java/com/noisefit/ui/onboarding/pairing/find/FindDeviceListFragment.kt
+++ app/src/main/java/com/noisefit/ui/onboarding/pairing/find/FindDeviceListFragment.kt
@@ -40,7 +40,7 @@ import com.noisefit.ui.onboarding.pairing.PairDeviceActivity
 import com.noisefit.ui.onboarding.pairing.pair.NearbyDevicesAdapter
 import com.noisefit.ui.onboarding.pairing.pair.NearbyDevicesClickListener
 import com.noisefit.util.ApplicationUtils
-import com.noisefit.util.DeviceUtil
+
 import com.noisefit_commans.data.BinaryActionCallback
 import com.noisefit_commans.data.ErrorResponse
 import com.noisefit_commans.data.UIComponentType
@@ -81,8 +81,7 @@ class FindDeviceListFragment :
     private var bluetoothLeScanner: BluetoothLeScanner? = null
 
 
-    @Inject
-    lateinit var deviceUtil: DeviceUtil
+
 
     private var scanCount = 0
 
