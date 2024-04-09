585beeeb70393df87d17e8aaa0bbbf208b8d92f5
brianjolly1 <devapp411@gmail.com>
2024-04-09 12:16:21 +0530

    OS-1874(msg change)

--- app/src/main/java/com/oreo/ui/home/summary/paginate/SummaryDataFragmentToday.kt
+++ app/src/main/java/com/oreo/ui/home/summary/paginate/SummaryDataFragmentToday.kt
@@ -696,6 +696,7 @@ class SummaryDataFragmentToday :
                     requireContext(),
                     it.second?.ringInfo?.image2
                 )
+
                 if (viewModel.checkBeforeTime()) {
                     binding.contentMain.lytChargeRing.textView84.text =
                         getString(R.string.text_before_9_pm_battery_charge_msg)
