f7dfaaa86255b8b846de6e4e815bff6af76901df
deepak.k2 <deepak.k@nexxbase.com>
2024-04-10 14:07:01 +0530

    stress test code removed

--- app/src/main/java/com/oreo/ui/home/summary/OSummaryFragment.kt
+++ app/src/main/java/com/oreo/ui/home/summary/OSummaryFragment.kt
@@ -184,10 +184,7 @@ class OSummaryFragment : BaseFragment<FragmentSummaryOBinding>(FragmentSummaryOB
 
         binding.lytHeader.profileView1.setOnClickListener {
             viewModel.sessionManager.logMoEngageAppEvent(MoEngageLunaAppEvents.luna_hamburger_click)
-//            navigate(R.id.OMyProfileFragment)
-            navigate(R.id.stressInternalParentOreo, Bundle().apply {
-                putString("date", mainViewModel.selectedDate)
-            })
+            navigate(R.id.OMyProfileFragment)
         }
 
         /* setFragmentResultListener(ADD_WORKOUT_REQUEST_KEY) { _, bundle ->
