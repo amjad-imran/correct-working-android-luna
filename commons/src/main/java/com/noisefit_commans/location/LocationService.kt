8726287bd417213beb144ca76a4284d6adf9443c
deepak.k2 <deepak.k@nexxbase.com>
2024-04-17 16:22:56 +0530

    clear notification

--- commons/src/main/java/com/noisefit_commans/location/LocationService.kt
+++ commons/src/main/java/com/noisefit_commans/location/LocationService.kt
@@ -1,18 +1,15 @@
 package com.noisefit_commans.location
 
-import android.R.attr.data
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.Service
 import android.content.Context
 import android.content.Intent
-import android.nfc.NfcAdapter.EXTRA_DATA
 import android.os.Build
 import android.os.IBinder
 import androidx.core.app.NotificationCompat
 import androidx.lifecycle.MutableLiveData
-import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.google.android.gms.location.LocationServices
 import com.noisefit_commans.R
 import com.noisefit_commans.data.db.LocationModel
@@ -25,14 +22,9 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
-import kotlinx.coroutines.channels.BroadcastChannel
-import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
 import javax.inject.Inject
 
 
@@ -73,7 +65,7 @@ class LocationService : Service() {
         val notification = NotificationCompat.Builder(this, CHANNEL_ID)
             .setContentTitle("Tracking location...")
             //.setContentText("Location: null")
-            .setSmallIcon(R.drawable.ic_map_loc_start)
+            .setSmallIcon(R.drawable.icon_transparent)
             .setOngoing(true)
 
         val notificationManager =
@@ -114,12 +106,12 @@ class LocationService : Service() {
 
                 locationBroadCast.postValue(Event(Pair(lat, long)))
 
-                val updatedNotification = notification.setContentText(
-                    "Location: ($lat, $long)\nTime ${
-                        SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(Date())
-                    }"
-                )
-                notificationManager.notify(1, updatedNotification.build())
+                /* val updatedNotification = notification.setContentText(
+                     "Location: ($lat, $long)\nTime ${
+                         SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(Date())
+                     }"
+                 )
+                 notificationManager.notify(1, updatedNotification.build())*/
             }
             .launchIn(serviceScope)
 
@@ -128,6 +120,10 @@ class LocationService : Service() {
     }
 
     private fun stop() {
+        val notificationManager =
+            applicationContext.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.cancel(NOTIFICATION_ID)
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             stopForeground(STOP_FOREGROUND_REMOVE)
         } else {
