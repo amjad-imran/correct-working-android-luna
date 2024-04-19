330758199f1aa17b026ea0c1780314a869a1fc32
deepak.k2 <deepak.k@nexxbase.com>
2024-04-19 10:56:30 +0530

    map load changes

--- commons/src/main/java/com/noisefit_commans/utils/GoogleMapsUtil.kt
+++ commons/src/main/java/com/noisefit_commans/utils/GoogleMapsUtil.kt
@@ -24,271 +24,48 @@ import javax.inject.Inject
 class GoogleMapsUtil
 @Inject
 constructor() {
-
-
-    /*   suspend fun plotLocationModalGoogleMaps(
-           activity: Activity,
-           googleMap: GoogleMap,
-           result: List<GPSDataResponse>
-       ): Flow<LatLngBounds> {
-           return flow {
-               val startingLtnLng = LatLng(result[0].latitude, result[0].longitude)
-               withContext(Dispatchers.Main) {
-                   googleMap.addMarker(
-                       MarkerOptions()
-                           .position(startingLtnLng)
-                           .draggable(false)
-                           .flat(false)
-                           .icon(
-                               BitmapDescriptorFactory.fromBitmap(
-                                   createStoreMarker(
-                                       activity,
-                                       "",
-                                       R.drawable.baseline_place_red_500_24dp
-                                   )
-                               )
-                           )
-                   )
-               }
-
-               val builder = LatLngBounds.Builder()
-               builder.include(startingLtnLng)
-               for (i in 1 until result.size) {
-                   val currentLtnLng = LatLng(result[i].latitude, result[i].longitude)
-                   val lastLtnLng = LatLng(result[i - 1].latitude, result[i - 1].longitude)
-                   builder.include(currentLtnLng)
-                   val color = "#ffcc0000"
-
-                   val options =
-                       PolylineOptions()
-                           .width(10f)
-                           .color(Color.parseColor(color))
-                           .geodesic(true)
-
-                   options.add(lastLtnLng)
-                   options.add(currentLtnLng)
-                   withContext(Dispatchers.Main) {
-                       val line = googleMap.addPolyline(options)
-                       line.endCap = RoundCap()
-                   }
-
-               }
-               if (result.size > 1) {
-                   val lastLtnLng = LatLng(
-                       result[result.size - 1].latitude,
-                       result[result.size - 1].longitude
-                   )
-                   withContext(Dispatchers.Main) {
-                       googleMap.addMarker(
-                           MarkerOptions()
-                               .position(lastLtnLng)
-                               .draggable(false)
-                               .flat(false)
-                               .icon(
-                                   BitmapDescriptorFactory.fromBitmap(
-                                       createStoreMarker(
-                                           activity,
-                                           "",
-                                           R.drawable.baseline_place_green_500_24dp
-                                       )
-                                   )
-                               )
-                       )
-                   }
-
-               }
-
-               emit(builder.build())
-
-           }
-       }*/
-
-    suspend fun plotLocationCyclingGoogleMaps(
-        activity: Activity,
-        googleMap: GoogleMap,
-        result: ArrayList<DoubleArray>
-    ): Flow<LatLngBounds> {
-        return flow {
-            val startingLatLng = result[0]
-            val builder = LatLngBounds.Builder()
-            if (!startingLatLng[0].isNaN() && !startingLatLng[1].isNaN()) {
-                val startingLtnLng = LatLng(startingLatLng[0], startingLatLng[1])
-                withContext(Dispatchers.Main) {
-                    googleMap.addMarker(
-                        MarkerOptions().position(startingLtnLng)
-                            .draggable(false)
-                            .flat(false)
-                            .icon(
-                                BitmapDescriptorFactory.fromBitmap(
-                                    createStoreMarker(
-                                        activity,
-                                        "",
-                                        R.drawable.ic_map_loc_start
-                                    )
-                                )
-                            )
-                    )
-                }
-
-                builder.include(startingLtnLng)
-            }
-//            LOGS.d("GPSSSSS ${Gson().toJson(result)}")
-            for (i in 1 until result.size) {
-
-                val current = result[i]
-//                LOGS.d("GPSSSSS ${current[2].toBigDecimal()}")
-                val last = result[i - 1]
-                var currentLtnLng: LatLng? = null
-                var lastLtnLng: LatLng? = null
-                if (!current[0].isNaN() && !current[1].isNaN()) {
-                    currentLtnLng = LatLng(current[0], current[1])
-                    builder.include(currentLtnLng)
-                }
-
-                if (!last[0].isNaN() && !last[1].isNaN()) {
-                    lastLtnLng = LatLng(last[0], last[1])
-                }
-
-                if (currentLtnLng != null && lastLtnLng != null) {
-                    val color = "#ca99ff"
-                    val options =
-                        PolylineOptions()
-                            .width(10f)
-                            .color(Color.parseColor(color))
-                            .geodesic(true)
-
-                    options.add(lastLtnLng)
-                    options.add(currentLtnLng)
-                    withContext(Dispatchers.Main) {
-                        val line = googleMap.addPolyline(options)
-                        line.endCap = RoundCap()
-                    }
-
-                }
-
-            }
-            if (result.size > 1) {
-                val current = result[result.size - 1]
-                if (!current[0].isNaN() && !current[1].isNaN()) {
-                    val currentLtnLng = LatLng(current[0], current[1])
-                    withContext(Dispatchers.Main) {
-                        googleMap.addMarker(
-                            MarkerOptions()
-                                .position(currentLtnLng)
-                                .draggable(false)
-                                .flat(false)
-                                .icon(
-                                    BitmapDescriptorFactory.fromBitmap(
-                                        createStoreMarker(
-                                            activity,
-                                            "",
-                                            R.drawable.ic_map_loc_end
-                                        )
-                                    )
-                                )
-                        )
-                    }
-
-                }
-
-
-            }
-
-            emit(builder.build())
-
-        }
-    }
-
     suspend fun plotLocationModalGoogleMaps(
         activity: Activity,
         googleMap: GoogleMap,
-        locationData: List<LocationDataNetwork>
+        result: List<LocationDataNetwork>
     ): Flow<LatLngBounds> {
         return flow {
-            val startingLatLng = locationData.first()
             val builder = LatLngBounds.Builder()
-            if (!startingLatLng.lat!!.isNaN() && !startingLatLng.long!!.isNaN()) {
-                val startingLtnLng = LatLng(startingLatLng.lat, startingLatLng.long)
-                withContext(Dispatchers.Main) {
-                    googleMap.addMarker(
-                        MarkerOptions().position(startingLtnLng)
-                            .draggable(false)
-                            .flat(false)
-                            .icon(
-                                BitmapDescriptorFactory.fromBitmap(
-                                    createStoreMarker(
-                                        activity,
-                                        "",
-                                        R.drawable.ic_map_loc_start
-                                    )
-                                )
+            val color = "#8ed3f1"
+            val myPolylineOptionsInstance = PolylineOptions()
+                .width(10f)
+                .color(Color.parseColor(color))
+
+            for (i in result.indices) {
+                if (result[i].lat != null && result[i].long != null) {
+                    val currentLtnLng = LatLng(result[i].lat!!, result[i].long!!)
+                    if (i == 0) {
+                        withContext(Dispatchers.Main) {
+                            addMarker(
+                                googleMap,
+                                currentLtnLng,
+                                activity,
+                                R.drawable.ic_map_loc_start
                             )
-                    )
-                }
-
-                builder.include(startingLtnLng)
-            }
-            for (i in 1 until locationData.size) {
-
-                val current = locationData[i]
-                val last = locationData[i - 1]
-                var currentLtnLng: LatLng? = null
-                var lastLtnLng: LatLng? = null
-                if (!current.lat!!.isNaN() && !current.long!!.isNaN()) {
-                    currentLtnLng = LatLng(current.lat, current.long)
-                    builder.include(currentLtnLng)
-                }
-
-                if (!last.lat!!.isNaN() && !last.long!!.isNaN()) {
-                    lastLtnLng = LatLng(last.lat, last.long)
-                }
-
-                if (currentLtnLng != null && lastLtnLng != null) {
-                    val color = "#8ed3f1"
-                    val options =
-                        PolylineOptions()
-                            .width(10f)
-                            .color(Color.parseColor(color))
-                            .geodesic(true)
-
-                    options.add(lastLtnLng)
-                    options.add(currentLtnLng)
-                    withContext(Dispatchers.Main) {
-                        val line = googleMap.addPolyline(options)
-                        line.endCap = RoundCap()
-                        if (i == 1) {
-                            line.startCap = RoundCap()
+                        }
+                    } else if (i == (result.size - 1)) {
+                        withContext(Dispatchers.Main) {
+                            addMarker(
+                                googleMap,
+                                currentLtnLng,
+                                activity,
+                                R.drawable.ic_map_loc_end
+                            )
                         }
                     }
 
+                    myPolylineOptionsInstance.add(currentLtnLng)
+                    builder.include(currentLtnLng)
                 }
-
             }
-            if (locationData.size > 1) {
-                val current = locationData[locationData.size - 1]
-                if (!current.lat!!.isNaN() && !current.long!!.isNaN()) {
-                    val currentLtnLng = LatLng(current.lat, current.long)
-                    withContext(Dispatchers.Main) {
-                        googleMap.addMarker(
-                            MarkerOptions()
-                                .position(currentLtnLng)
-                                .draggable(false)
-                                .flat(false)
-                                .icon(
-                                    BitmapDescriptorFactory.fromBitmap(
-                                        createStoreMarker(
-                                            activity,
-                                            "",
-                                            R.drawable.ic_map_loc_end
-                                        )
-                                    )
-                                )
-                        )
-                    }
 
-                }
-
-
+            withContext(Dispatchers.Main) {
+                googleMap.addPolyline(myPolylineOptionsInstance)
             }
 
             emit(builder.build())
@@ -296,6 +73,28 @@ constructor() {
         }
     }
 
+    private fun addMarker(
+        googleMap: GoogleMap,
+        currentLtnLng: LatLng,
+        activity: Activity,
+        drawable: Int
+    ) {
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(currentLtnLng)
+                .draggable(false)
+                .flat(false)
+                .icon(
+                    BitmapDescriptorFactory.fromBitmap(
+                        createStoreMarker(
+                            activity,
+                            "",
+                            drawable
+                        )
+                    )
+                )
+        )
+    }
 
     private fun createStoreMarker(activity: Activity, text: String, drawable: Int): Bitmap {
         val markerLayout: View = activity.layoutInflater.inflate(R.layout.layout_marker, null)
