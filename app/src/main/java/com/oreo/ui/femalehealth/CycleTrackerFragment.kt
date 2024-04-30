f0dd7188864dfa1128d35bea0d2fd736e6713bfb
deepak.k2 <deepak.k@nexxbase.com>
2024-04-30 14:49:14 +0530

    Cycle tracker design init

---
+++ app/src/main/java/com/oreo/ui/femalehealth/CycleTrackerFragment.kt
@@ -0,0 +1,18 @@
+package com.oreo.ui.femalehealth
+
+import com.noisefit.luna.databinding.FragmentCycleTrackerBinding
+import com.noisefit_commans.ui.BaseFragment
+
+
+class CycleTrackerFragment :
+    BaseFragment<FragmentCycleTrackerBinding>(FragmentCycleTrackerBinding::inflate) {
+    override fun initListener() {
+
+    }
+
+    override fun subscribeObservers() {
+
+    }
+
+
+}
