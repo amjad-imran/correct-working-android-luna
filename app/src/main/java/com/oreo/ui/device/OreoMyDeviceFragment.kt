d97c3713482594f49bfd6c51c91f89d814af20a9
deepak.k2 <deepak.k@nexxbase.com>
2024-05-15 16:26:10 +0530

    anim change, G fit removed from device

--- app/src/main/java/com/oreo/ui/device/OreoMyDeviceFragment.kt
+++ app/src/main/java/com/oreo/ui/device/OreoMyDeviceFragment.kt
@@ -132,9 +132,9 @@ class OreoMyDeviceFragment :
             mViewModel.sessionManager.logMoEngageAppEvent(MoEngageLunaAppEvents.luna_mydevices_about_click)
             navigate(R.id.OAboutDeviceFragment)
         }
-        binding.rowGoogleFit.setOnClickListener {
+        /*binding.rowGoogleFit.setOnClickListener {
             navigate(R.id.googleFitFragmentOreo)
-        }
+        }*/
 
         binding.rowWarrantyRegistration.setOnClickListener {
             //navigate(R.id.warrantyFragmentOreo)
