02cc1ff61759c73072417bdf77817d7df4010d9c
deepak.k2 <deepak.k@nexxbase.com>
2024-09-02 12:28:13 +0530

    find my ring init

---
+++ app/src/main/java/com/oreo/ui/findmyring/RingLocationFragment.kt
@@ -0,0 +1,16 @@
+package com.oreo.ui.findmyring
+
+import com.noisefit.luna.databinding.FragmentRingLocationBinding
+import com.noisefit_commans.ui.BaseFragment
+
+class RingLocationFragment :
+    BaseFragment<FragmentRingLocationBinding>(FragmentRingLocationBinding::inflate) {
+
+    override fun initListener() {
+
+    }
+
+    override fun subscribeObservers() {
+
+    }
+}
