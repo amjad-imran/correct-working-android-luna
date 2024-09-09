e0849b3166c1a005c94a14a9079d545f8577994a
deepak.k2 <deepak.k@nexxbase.com>
2024-09-09 18:16:24 +0530

    location message change

--- app/src/main/java/com/oreo/ui/device/BottomSheetLocationPermissionFindMyRing.kt
+++ app/src/main/java/com/oreo/ui/device/BottomSheetLocationPermissionFindMyRing.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.View
 import androidx.core.os.bundleOf
 import androidx.fragment.app.setFragmentResult
-import com.noisefit.luna.databinding.BottomSheetLocationPermBinding
 import com.noisefit.luna.databinding.BottomSheetLocationPermFindMyRingBinding
 import com.noisefit_commans.ui.BaseBottomSheetWithTransparent
 import dagger.hilt.android.AndroidEntryPoint
