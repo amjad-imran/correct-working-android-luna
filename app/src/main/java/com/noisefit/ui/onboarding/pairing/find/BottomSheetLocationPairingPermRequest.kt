35ee632ff0ed483246fb6e770614ce35581138d5
deepak.k2 <deepak.k@nexxbase.com>
2024-11-13 12:52:31 +0530

    Per content and design changes

--- app/src/main/java/com/noisefit/ui/onboarding/pairing/find/BottomSheetLocationPairingPermRequest.kt
+++ app/src/main/java/com/noisefit/ui/onboarding/pairing/find/BottomSheetLocationPairingPermRequest.kt
@@ -6,24 +6,24 @@ import androidx.core.os.bundleOf
 import androidx.fragment.app.setFragmentResult
 import com.noisefit.luna.R
 import com.noisefit.luna.databinding.BottomSheetLocationPermBinding
+import com.noisefit.luna.databinding.BottomSheetLocationPermFindMyRingBinding
 import com.noisefit_commans.ui.BaseBottomSheetWithTransparent
 import com.noisefit_commans.ui.gone
+import com.oreo.ui.device.BottomSheetLocationPermissionFindMyRing
 import dagger.hilt.android.AndroidEntryPoint
 
 const val LOCATION_PERM_REQUEST_PAIRING = "LOCATION_PERM_REQUEST_PAIRING"
 
 @AndroidEntryPoint
 class BottomSheetLocationPermissionRequest :
-    BaseBottomSheetWithTransparent<BottomSheetLocationPermBinding>(
-        BottomSheetLocationPermBinding::inflate
+    BaseBottomSheetWithTransparent<BottomSheetLocationPermFindMyRingBinding>(
+        BottomSheetLocationPermFindMyRingBinding::inflate
     ) {
-
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.tvDescription2.gone()
-        binding.dot2.gone()
+        binding.tvTitle.text = getString(R.string.text_permission_to_use_your_location)
         binding.tvDescription.text = getString(R.string.text_when_using_bluetooth_to)
     }
 
@@ -34,13 +34,13 @@ class BottomSheetLocationPermissionRequest :
             )
             navigateUpSafe()
         }
+
         binding.btnNo.setOnClickListener {
             setFragmentResult(
                 LOCATION_PERM_REQUEST_PAIRING, bundleOf("allow" to false)
             )
             navigateUpSafe()
         }
-
     }
 
     override fun subscribeObservers() {
