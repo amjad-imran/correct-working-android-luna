6aac615812e44a43241c230e71403cdf9dc89b73
deepak.k2 <deepak.k@nexxbase.com>
2025-02-19 10:15:37 +0530

    breathing hide toolbar back

--- app/src/main/java/com/oreo/ui/sleep2/sleepplanner/exercise/BreathingExerciseFragment.kt
+++ app/src/main/java/com/oreo/ui/sleep2/sleepplanner/exercise/BreathingExerciseFragment.kt
@@ -93,6 +93,7 @@ class BreathingExerciseFragment :
 
         binding.lytStateCompleted.btnRestart.setOnClickListener {
             binding.lytStateCompleted.root.gone()
+            binding.lytToolbar.root.visible()
             startLottie()
         }
     }
@@ -111,6 +112,7 @@ class BreathingExerciseFragment :
             }
 
             override fun onAnimationEnd(animation: Animator) {
+                binding.lytToolbar.root.gone()
                 binding.lottieAnimationView.gone()
                 binding.imageBack.visible()
                 binding.tvTimer.gone()
