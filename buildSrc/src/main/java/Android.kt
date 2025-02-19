0742927a066cf9391fe4059949c2be155d982d14
deepak.k2 <deepak.k@nexxbase.com>
2025-02-19 17:38:27 +0530

    congrats text color and background

--- buildSrc/src/main/java/Android.kt
+++ buildSrc/src/main/java/Android.kt
@@ -7,6 +7,6 @@ object Android {
     const val minSdk = 23
 
     const val targetSdk = 34
-    const val versionCode = 114
+    const val versionCode = 115
     const val versionName = "1.3.7.16.old"
 }
