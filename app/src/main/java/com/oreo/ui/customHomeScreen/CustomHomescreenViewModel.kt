4eb034582c230c350caba9eb465845b9ebf28010
deepak.k2 <deepak.k@nexxbase.com>
2025-04-24 18:07:09 +0530

    Revert "Caffeine Intake item added in CustomHomescreenFrag"
    This reverts commit 4ed0e476c11fbbd1cb621a6d4905a4902b80cf34.

--- app/src/main/java/com/oreo/ui/customHomeScreen/CustomHomescreenViewModel.kt
+++ app/src/main/java/com/oreo/ui/customHomeScreen/CustomHomescreenViewModel.kt
@@ -208,41 +208,33 @@ class CustomHomescreenViewModel @Inject constructor(
                 1
             )
 
-            this["caffeine_intake "] = CustomHomeScreenItem(
-                R.drawable.icon_caffeine_intake,
-                "caffeine_intake",
-                resourceProvider.getString(R.string.text_caffeine_intake),
-                true,
-                2
-            )
-
             this["activity"] = CustomHomeScreenItem(
                 R.drawable.icon_activity,
                 "activity",
                 resourceProvider.getString(R.string.text_activity_o),
                 true,
-                3
+                2
             )
             this["readiness"] = CustomHomeScreenItem(
                 R.drawable.icon_readiness,
                 "readiness",
                 resourceProvider.getString(R.string.text_readiness),
                 true,
-                4
+                3
             )
             this["sleep_planner"] = CustomHomeScreenItem(
                 R.drawable.icon_sleep_planner,
                 "sleep_planner",
                 resourceProvider.getString(R.string.text_sleep_planner),
                 true,
-                5
+                4
             )
             this["heart_rate"] = CustomHomeScreenItem(
                 R.drawable.icon_heart_rate,
                 "heart_rate",
                 resourceProvider.getString(R.string.text_heart_rate),
                 true,
-                6
+                5
             )
 
 //        this["health_monitor"] = CustomHomeScreenItem(
@@ -257,21 +249,21 @@ class CustomHomescreenViewModel @Inject constructor(
                 "daily_goals",
                 resourceProvider.getString(R.string.text_daily_goals),
                 true,
-                7
+                6
             )
             this["luna_ai"] = CustomHomeScreenItem(
                 R.drawable.icon_luna_ai,
                 "luna_ai",
                 resourceProvider.getString(R.string.text_luna_ai),
                 true,
-                8
+                7
             )
             this["stress"] = CustomHomeScreenItem(
                 R.drawable.icon_stress,
                 "stress",
                 resourceProvider.getString(R.string.text_stress),
                 true,
-                9
+                8
             )
 
             if (shouldShowFemaleHealth()) {
@@ -280,7 +272,7 @@ class CustomHomescreenViewModel @Inject constructor(
                     "cycle_tracker",
                     resourceProvider.getString(R.string.text_cycle_tracker),
                     true,
-                    10
+                    9
                 )
             }
             this["7_day_trends_card"] = CustomHomeScreenItem(
@@ -288,15 +280,16 @@ class CustomHomescreenViewModel @Inject constructor(
                 "7_day_trends_card",
                 resourceProvider.getString(R.string.text_7_day_trends_cards),
                 true,
-                11
+                10
             )
             this["workout_history"] = CustomHomeScreenItem(
                 R.drawable.icon_flexibility_training,
                 "workout_history",
                 resourceProvider.getString(R.string.text_workout_history),
                 true,
-                12
+                11
             )
+
 
         }
 
