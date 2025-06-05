23f6f258819df557c91bc8b678fae0fb7c4d6415
Yash Singh <hustleryash25@gmail.com>
2025-06-05 16:15:14 +0530

    luna managed - toggle fixed

--- app/src/main/java/com/oreo/ui/customHomeScreen/CustomHomescreenViewModel.kt
+++ app/src/main/java/com/oreo/ui/customHomeScreen/CustomHomescreenViewModel.kt
@@ -110,9 +110,7 @@ class CustomHomescreenViewModel @Inject constructor(
         viewModelScope.launch {
 
             if (!canMakeApiCall()) {
-                lunaManagedState.postValue(
-                    localDataSource.getCustomHomeScreenItemsPriorityList()?.manage?:true
-                )
+                lunaManagedState.postValue(lunaManagedState.value)
                 return@launch
             }
 
@@ -165,6 +163,7 @@ class CustomHomescreenViewModel @Inject constructor(
                                 localDataSource.setCustomHomeScreenItemsPriorityList(
                                     customHomeScreenData
                                 )
+                                lunaManagedState.postValue(isToggleOn)
                                 dataUpdated.postValue(Event(true))
                             }
                         }
