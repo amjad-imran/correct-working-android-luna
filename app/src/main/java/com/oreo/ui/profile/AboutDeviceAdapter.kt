6556d526be8ee93acd4a90945e378d64cc282f3e
Yash Singh <hustleryash25@gmail.com>
2025-06-05 18:33:36 +0530

    OAboutDeviceFragment - Copy button added for Serial Number

--- app/src/main/java/com/oreo/ui/profile/AboutDeviceAdapter.kt
+++ app/src/main/java/com/oreo/ui/profile/AboutDeviceAdapter.kt
@@ -3,7 +3,9 @@ package com.oreo.ui.profile
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.noisefit.luna.R
 import com.noisefit.luna.databinding.RowOreoAboutDeviceBinding
+import com.noisefit_commans.common.copyToClipBoard
 import com.noisefit_commans.ui.gone
 import com.noisefit_commans.ui.visible
 
@@ -17,10 +19,23 @@ class AboutDeviceAdapter : RecyclerView.Adapter<AboutDeviceAdapter.ViewHolder>()
             binding.tvTitle.text = data.title
             binding.tvValue.text = data.value
 
+            val context = binding.root.context
+            if (data.title.equals(context.getString(R.string.text_serial_number))){
+                binding.ivCopy.visible()
+            }else{
+                binding.ivCopy.gone()
+            }
+
             if ((bindingAdapterPosition + 1) == itemCount) {
                 binding.divider.root.gone()
             } else {
                 binding.divider.root.visible()
+            }
+
+            binding.ivCopy.setOnClickListener {
+                data.value.copyToClipBoard(
+                    context.getString(R.string.text_copied)
+                )
             }
         }
 
