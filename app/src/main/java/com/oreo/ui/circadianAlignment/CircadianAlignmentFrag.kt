f36c532a121ad4a200257acce2c1e50451b1cc4f
Yash Singh <hustleryash25@gmail.com>
2025-07-15 17:27:48 +0530

    commit - circadian

--- app/src/main/java/com/oreo/ui/circadianAlignment/CircadianAlignmentFrag.kt
+++ app/src/main/java/com/oreo/ui/circadianAlignment/CircadianAlignmentFrag.kt
@@ -1,15 +1,11 @@
 package com.oreo.ui.circadianAlignment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.noisefit.luna.R
 import com.noisefit.luna.databinding.FragmentCircadianAlignmentBinding
-import com.noisefit.luna.databinding.LayoutDashCircadianBinding
 import com.noisefit_commans.ui.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -31,9 +27,9 @@ class CircadianAlignmentFrag :
     }
 
     private fun setUi() {
-        binding.toolbar.apply {
+        binding.toolbar.tvTitle.text = getString(R.string.text_circadian_alignment)
 
-        }
+
     }
 
     private fun setRecycler() {
@@ -42,7 +38,9 @@ class CircadianAlignmentFrag :
     }
 
     override fun initListener() {
-
+        binding.toolbar.backBtn.setOnClickListener {
+            navigateUpSafe()
+        }
     }
 
     override fun subscribeObservers() {
