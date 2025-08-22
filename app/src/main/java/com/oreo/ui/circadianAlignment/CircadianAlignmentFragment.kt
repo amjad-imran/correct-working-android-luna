6c94ca80316bf247dd26fe443f002d190a78c2bd
deepak.k2 <deepak.k@nexxbase.com>
2025-08-22 17:25:40 +0530

    OS-5713

--- app/src/main/java/com/oreo/ui/circadianAlignment/CircadianAlignmentFragment.kt
+++ app/src/main/java/com/oreo/ui/circadianAlignment/CircadianAlignmentFragment.kt
@@ -109,6 +109,7 @@ class CircadianAlignmentFragment :
         val endTime: String,
         val circadianMidpoint: String,
         val avgBefore: String,
+        val avgNow: String,
         /*var nudge: String?*/
     )
 
@@ -142,7 +143,7 @@ class CircadianAlignmentFragment :
                 .plusMinutes((60 - midEndDateTime.minute.toLong()))
 
             val circadianMidPointDateTime =
-                LocalDateTime.parse(circadianResponse.circadianMidpoint, formatter)
+                LocalDateTime.parse(circadianResponse.avgNow, formatter)
             val avgBeforeMidPointDateTime =
                 LocalDateTime.parse(circadianResponse.avgBefore, formatter)
 
@@ -538,6 +539,7 @@ class CircadianAlignmentFragment :
                 circadianMidPoint.endTime ?: "",
                 circadianMidPoint.circadianMidpoint ?: "",
                 circadianMidPoint.avgBefore ?: "",
+                circadianMidPoint.avgNow ?: "",
                 /*null,*/
             )
             setCircadianMidPointGraph(circadianMidPointResponse)
