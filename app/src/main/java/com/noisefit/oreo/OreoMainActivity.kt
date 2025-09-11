5eadfab4dda5dd2b31123aa09d75c21c646f1770
deepak.k2 <deepak.k@nexxbase.com>
2025-09-11 16:57:45 +0530

    OS-5986

--- app/src/main/java/com/noisefit/oreo/OreoMainActivity.kt
+++ app/src/main/java/com/noisefit/oreo/OreoMainActivity.kt
@@ -478,7 +478,7 @@ class OreoMainActivity : BaseActivity<ActivityOreoMainBinding>() {
 
         val lastDestination = navController?.currentDestination
 
-        /*if (lastDestination?.id == R.id.navigation_oreo_home) {
+        if (lastDestination?.id == R.id.navigation_oreo_home) {
             binding.lytAddWorkoutSelector.tvAddSleep.visible()
             binding.lytAddWorkoutSelector.ivRecordSleep.visible()
             animateItemsUp(binding.lytAddWorkoutSelector.tvAddSleep, 350f)
@@ -486,7 +486,7 @@ class OreoMainActivity : BaseActivity<ActivityOreoMainBinding>() {
         } else {
             binding.lytAddWorkoutSelector.tvAddSleep.gone()
             binding.lytAddWorkoutSelector.ivRecordSleep.gone()
-        }*/
+        }
 
         if (viewModel.shouldShowFemaleHealthCta() && lastDestination?.id == R.id.navigation_oreo_home) {
             binding.lytAddWorkoutSelector.ivLogPeriod.visible()
