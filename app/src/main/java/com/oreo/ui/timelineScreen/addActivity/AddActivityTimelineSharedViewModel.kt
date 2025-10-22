4d1df2aab151b8c4d3181e7b3e59bacf85a29f8e
Yash Singh <hustleryash25@gmail.com>
2025-10-22 10:27:57 +0530

    commit - https://gonoisefit.atlassian.net/browse/OS-6505

--- app/src/main/java/com/oreo/ui/timelineScreen/addActivity/AddActivityTimelineSharedViewModel.kt
+++ app/src/main/java/com/oreo/ui/timelineScreen/addActivity/AddActivityTimelineSharedViewModel.kt
@@ -122,7 +122,7 @@ class AddActivityTimelineSharedViewModel @Inject constructor(
         )
         list.add(
             AddActivityListTimelineModel(
-                name = resourcesProvider.getString(R.string.text_add_alcohol_intake),
+                name = resourcesProvider.getString(R.string.text_alcohol_intake),
                 type = AddActivityItemsEnum.ALCOHOL,
                 titleColor = "#BE7A64".toColorInt()
             )
