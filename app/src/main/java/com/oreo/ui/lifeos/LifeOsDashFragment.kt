38a8cd107b3d1e4b526262e573be48788d7cb8e1
deepak.k2 <deepak.k@nexxbase.com>
2025-11-05 15:27:02 +0530

    OS-6667 - life os dash ui

---
+++ app/src/main/java/com/oreo/ui/lifeos/LifeOsDashFragment.kt
@@ -0,0 +1,17 @@
+package com.oreo.ui.lifeos
+
+import com.noisefit.luna.databinding.FragmentLifeOsDashBinding
+import com.noisefit_commans.ui.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class LifeOsDashFragment :
+    BaseFragment<FragmentLifeOsDashBinding>(FragmentLifeOsDashBinding::inflate) {
+    override fun initListener() {
+
+    }
+
+    override fun subscribeObservers() {
+
+    }
+}
