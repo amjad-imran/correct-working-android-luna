1d3d4b8785f70eb45b31406c878383dec78cd9a2
Yash Singh <hustleryash25@gmail.com>
2025-11-16 13:01:41 +0530

    commit - https://gonoisefit.atlassian.net/browse/OS-6666

--- app/src/main/java/com/oreo/ui/lifeos/LifeOsDashFragment.kt
+++ app/src/main/java/com/oreo/ui/lifeos/LifeOsDashFragment.kt
@@ -2,7 +2,37 @@ package com.oreo.ui.lifeos
 
 import android.os.Bundle
 import android.view.View
-import androidx.core.graphics.toColorInt
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -79,12 +109,158 @@ class LifeOsDashFragment :
             return
         }
 
-        val progressView = binding.lytHeader.lytOnboardQuesProgress.angularProgress
-        progressView.trackColor = "#33FFFFFF".toColorInt()
-        progressView.progress = ansSize/quesSize.toFloat()
-
-//        progressView.animateProgressTo(0.4f)
         binding.lytHeader.lytOnboardQuesProgress.root.visible()
+
+        binding.lytHeader.lytOnboardQuesProgress.composeView.setContent {
+            GradientBorderCard(ansSize/quesSize.toFloat())
+        }
+    }
+
+    @Composable
+    private fun GradientBorderCard(curProgress: Float) {
+        val borderWidth = 2.dp
+        val cornerRadius = 16.dp
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .border(
+                    width = borderWidth,
+                    brush = Brush.linearGradient(
+                        colors = listOf(
+                            Color(0x1AFFFFFF),
+                            Color(0x00FFFFFF)
+                        )
+                    ),
+                    shape = RoundedCornerShape(cornerRadius)
+                )
+                .clip(RoundedCornerShape(cornerRadius - borderWidth))
+                .background(
+                    brush = Brush.linearGradient(
+                        colors = listOf(
+                            Color.White,
+                            Color.White.copy(alpha = 0.3f)
+                        )
+                    ),
+                    alpha = 0.01f
+                )
+        ) {
+            Column {
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Row(
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                )
+                {
+                    Image(
+                        painter = painterResource(R.drawable.ic_lifeos_star_small),
+                        contentDescription = null
+                    )
+
+                    Text(
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(horizontal = 16.dp),
+                        text = getString(R.string.text_get_more_from_life_os),
+                        style = TextStyle(
+                            color = Color.White,
+                            fontSize = 14.sp,
+                            fontFamily = FontFamily(Font(com.noisefit_commans.R.font.gilroy_medium)),
+                            platformStyle = PlatformTextStyle(includeFontPadding = false)
+                        )
+                    )
+
+                    Box(
+                        modifier = Modifier
+                            .border(
+                                width = 1.dp,
+                                color = Color(0x19FFFFFF),
+                                shape = CircleShape
+                            )
+                            .background(
+                                color = Color(0x1EFFFFFF),
+                                shape = CircleShape
+                            ),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Image(
+                            painter = painterResource(R.drawable.ic_arrow_planner_right),
+                            contentDescription = null,
+                            modifier = Modifier.padding(4.dp)
+                        )
+                    }
+                }
+
+                Spacer(Modifier.height(8.dp))
+
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp),
+                    text = getString(R.string.text_tell_us_how_you_live_work_and_recover_luna_will_personalise_your_experience_to_help_you_live_better),
+                    style = TextStyle(
+                        color = Color.White,
+                        fontSize = 12.sp,
+                        fontFamily = FontFamily(Font(com.noisefit_commans.R.font.gilroy_regular)),
+                        platformStyle = PlatformTextStyle(includeFontPadding = false)
+                    )
+                )
+
+                Spacer(Modifier.height(16.dp))
+
+                GradientProgressBar(curProgress)
+            }
+        }
+    }
+
+    @Composable
+    fun GradientProgressBar(curProgress: Float) {
+        val progress = curProgress.coerceIn(0f, 1f)
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(4.dp)
+                .background(
+                    color = Color.White.copy(alpha = 0.2f),
+                    shape = RoundedCornerShape(16.dp)
+                )
+                .clip(RoundedCornerShape(16.dp))
+        ) {
+            Canvas(
+                modifier = Modifier
+                    .matchParentSize()
+                    .alpha(0.8f)
+            ) {
+                val barWidth = size.width * progress
+
+                if (barWidth <= 0f) return@Canvas
+
+                val brush = Brush.linearGradient(
+                    colorStops = arrayOf(
+                        0.00f to Color(red = 1f, green = 1f, blue = 0.72f),
+                        0.29f to Color(red = 0.65f, green = 0.92f, blue = 0.97f),
+                        0.52f to Color(red = 0.68f, green = 0.64f, blue = 0.97f),
+                        0.75f to Color(red = 0.95f, green = 0.78f, blue = 0.94f),
+                        1.00f to Color(red = 1f, green = 1f, blue = 0.72f)
+                    ),
+                    start = Offset(0f, 0f),
+                    end = Offset(size.width, 0f)
+                )
+
+                drawRect(
+                    brush = brush,
+                    topLeft = Offset.Zero,
+                    size = Size(
+                        width = barWidth,
+                        height = size.height
+                    )
+                )
+            }
+        }
     }
 
     private fun setupQuestionsRecycler() {
