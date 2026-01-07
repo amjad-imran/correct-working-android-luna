7fedae2d98f9ea6ad20078b75f3e3a52f7385f23
Yash Singh <hustleryash25@gmail.com>
2026-01-07 16:49:26 +0530

    commit - X Axis Fix - Insight Details Page

--- app/src/main/java/com/oreo/ui/lifeos/LifeOsInsightDetailsFragment.kt
+++ app/src/main/java/com/oreo/ui/lifeos/LifeOsInsightDetailsFragment.kt
@@ -285,7 +285,7 @@ class LifeOsInsightDetailsFragment :
                     yAxisRange = data.yAxisRange!!,
                     maxValue = data.maxValue!!,
                     selectedPosition = -1,
-                    null
+                    data.xAxisRangeInsights
                 )
                 v
 
