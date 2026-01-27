03c7c70485a25c53714c2778b66bf7fef30f21f1
Yash Singh <hustleryash25@gmail.com>
2026-01-27 19:20:55 +0530

    Your Habits -> date check added

--- app/src/main/java/com/oreo/ui/timelineScreen/TimelineScreenFragment.kt
+++ app/src/main/java/com/oreo/ui/timelineScreen/TimelineScreenFragment.kt
@@ -609,7 +609,10 @@ class TimelineScreenFragment :
                     LOGS.w("Loading more data")
                 }
 
-                viewModel.getUserSavedHabits(mainViewModel.selectedDate)
+                viewModel.getUserSavedHabits(
+                    mainViewModel.selectedDate,
+                    mainViewModel.selectedDate.equals(todayDate.minusDays(1).toString())
+                )
             }
         })
 
